9aacf31a8a8edee65c84927c45d15e24958b2eae
Akash29031994 <54771853+Akash29031994@users.noreply.github.com>
2019-09-18 17:48:04 +0530

    Adding API key

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,7 +36,7 @@ import { ExitGuard } from './com/concretio/commons/guards/exit-guard';
     AppRoutingModule,
     AngularFontAwesomeModule,
     AgmCoreModule.forRoot({
-      apiKey: '<APIKEY>'
+      apiKey: 'AIzaSyDhyDktSigXO_CMURhh-m4x4_YWyvUvnzM'
     }),
     NgMultiSelectDropDownModule.forRoot()
   ],
